a040b58f94736a107e22156bbdbda28b26c46c66
Evgeniy Taraasov <tarasovem@gmail.com>
2023-10-18 12:53:51 +0300

    Добавляет частичный функционал сбора данных из формы

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import '../css/style.css';
 
-function showModal () {
+function showModal() {
 	const backdrop = document.createElement('div');
 	const modal = document.querySelector('#modal');
 	backdrop.classList.add('modal__backdrop');
@@ -8,14 +8,19 @@ function showModal () {
 	modal.classList.add('show');
 }
 
-function closeModal (evt) {
+function closeModal(evt) {
 	const backdrop = document.querySelector('.modal__backdrop');
 	const modal = document.querySelector('#modal');
 
-	if (evt.target.classList.contains('modal') || evt.target.classList.contains('modal__close')) {
+	if (evt.target.matches('.modal, .modal__close, .modal__cancel')) {
 		backdrop.parentNode.removeChild(backdrop);
 		modal.classList.remove('show');
 	}
+}
+
+function submitForm() {
+	let formData = new FormData('#modal-form');
+	console.log(formData);
 }
 
 document.querySelector('#table').innerHTML = `
@@ -59,11 +64,24 @@ document.querySelector('#table').innerHTML = `
 				<button class="modal__close" type="button" />
 			</div>
 			<div class="modal__body">
-				Content
+				<form id="modal-form">
+					<div class="modal__form-control">
+						<label for="modal-title">Title</label>
+						<input type="text" id="modal-title" name="title" />
+					</div>
+					<div class="modal__form-control">
+						<label for="modal-price">Price</label>
+						<input type="number" id="modal-price" placeholder="0.00" min="0.00" step="0.01" pattern="\\d+(\\.\\d{2})?" name="price" />
+					</div>
+					<div class="modal__form-control">
+						<label for="modal-date">Date and time</label>
+						<input type="datetime-local" id="modal-date" name="dateAndTime" />
+					</div>
+				</form>
 			</div>
 			<div class="modal__footer">
 				<button class="modal__cancel" type="button">Close</button>
-				<button class="modal__add" type="button">Add</button>
+				<button class="modal__add" type="button" id="form-submit">Add</button>
 			</div>
 		</div>
 	</div>
@@ -78,3 +96,5 @@ document.querySelector('#table-add').addEventListener('click', () => {
 	showModal();
 	document.querySelector('#modal').addEventListener('click', closeModal);
 });
+
+document.querySelector('#form-submit').addEventListener('click', submitForm);
